cabe0a45ba8eeb5e4aabd9a0578c4fa5ec4a78ba
romanhalaichuk <vi2virbien@gmail.com>
2024-07-02 03:12:40 +0200

    Copy files from main to gh-pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TableManager from "./components/TableManager";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<TableManager />
+		</div>
+	);
+}
+
+export default App;
