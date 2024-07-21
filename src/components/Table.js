af50ccd974cee5a48f429cb40861043c07e40068
romanhalaichuk <vi2virbien@gmail.com>
2024-07-21 18:03:13 +0200

    ujed

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,13 +6,21 @@ const Table = ({ id, name, status, onTableClick }) => {
 		onTableClick(id);
 	};
 
+	const generateTableName = () => {
+		if (status === "free") {
+			return `Table ${id}`;
+		} else {
+			return name;
+		}
+	};
+
 	return (
 		<div
 			className={`table ${
 				status === "occupied" ? "table-occupied" : "table-free"
 			} ${status === "special" ? "table-special" : ""}`}
 			onClick={handleClick}>
-			<span className="table-name">{name}</span>
+			<span className="table-name">{generateTableName()}</span>
 		</div>
 	);
 };
